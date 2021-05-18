c95b95f9b7e965d0a513f2d92995233cffd70680
Thomas Krofta <tkrofta@outlook.com>
2021-05-18 12:20:36 +0200

    Repo Cleanup

--- visualization/script.sh
+++
@@ -1,6 +0,0 @@
-curl -XPOST localhost:8086/api/v2/query -sS \
-  -H 'Accept:application/csv' \
-  -H 'Content-type:application/vnd.flux' \
-  -d 'from(bucket:"signalk_metrics")
-        |> range(start:-5m)
-        |> filter(fn:(r) => r._measurement == "cpu")'
